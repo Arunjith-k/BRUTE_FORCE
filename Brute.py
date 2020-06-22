497fe699a87dc6e7eb78f08a62f7aee49b6e65a5
jackjocke <66721148+jackjocke@users.noreply.github.com>
2020-06-22 17:32:28 +0530

    Update Brute.py

--- Brute.py
+++ Brute.py
@@ -3,6 +3,18 @@ smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 
+print("   _____         _       ")
+print("  | __  |___ _ _| |_ ___ ")
+print("  | __ -|  _| | |  _| -_|")
+print("  |_____|_| |___|_| |___|\n")
+print("version :V1.0 cretaed by jackjocke....")
+print("a simple gmail bruteforcer...")
+print("www.github.com/jackjocke" )
+
+print('\n')
+
+
+
 user = raw_input("enter the targets email adress :")
 passwfile = raw_input("enter th password file name :")
 passwfile = open(passwfile, "r")
